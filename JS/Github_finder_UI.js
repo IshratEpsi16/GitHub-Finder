73cd5d5edb977d80a4e148b82fab7ae7a15a5273
Ishrat_Epsi <ishratepsi16@gmail.com>
2021-04-25 23:12:49 +0600

    First Commit

---
+++ JS/Github_finder_UI.js
@@ -0,0 +1,78 @@
+class UI {
+    constructor() {
+        this.profile = document.querySelector('#profile')
+    }
+
+    show_user_profile(user) {
+        this.clearAlert()
+        this.profile.innerHTML = `
+        <div class='container'>
+            <nav class="navbar navbar-dark bg-success mt-2">
+            <h2 class='text-white p-2 '>User Profile</h2>
+            </nav>
+        <div class="card card-body mb-3">
+        <div class='row'>
+        <div class='col-md-3'>   
+        <img src="${user.avatar_url}" class='mb-4'height=240px weight=240px>
+        <button class="bg-success">
+            <a href="${user.html_url}" class='text-white p-1 btn-block' style='text-decoration:none;' target='_blank'>
+                View User Profile
+            </a>
+            
+        </button>
+        </div>
+        <div class='col-md-9'>
+        <button class="bg-secondary text-white p-1"  target='_blank'>
+            Followers : ${user.followers}
+   
+        </button>
+        <button class="bg-success text-white p-1" target='_blank'>
+            Following : ${user.following}
+        </button>
+        <button class="bg-danger text-white p-1" target='_blank'>
+            
+                Public Gists : ${user.public_gists}
+            
+        </button>
+        <button class="bg-warning text-dark p-1" target='_blank'>
+        
+                Repository : ${user.public_repos}
+            
+            
+        
+        </button><br><br>
+        <ul class='list-group'>
+            <li class='list-group-item'>Company : ${user.company}</li>
+            <li class='list-group-item'>Blog : <a href="${user.blog}" style="text-decoration:none;">Blog link</a></li>
+            <li class='list-group-item'>Location : ${user.location}</li>
+            <li class='list-group-item'>Member Since : ${user.created_at}</li>
+        </ul>
+        </div>
+        </div>
+        </div>
+        </div>
+        `                               //since we didn't write any code in html's profile div.we will write all the codes here
+
+    }
+    clear_profile() {
+        this.profile.innerHTML = ''
+    }
+    showAlert(message, className) {
+        this.clearAlert()
+        this.clear_profile()
+        let div = document.createElement('div')
+        div.className = className
+        div.appendChild(document.createTextNode(message))
+        let container = document.querySelector('.container')
+        let search = document.querySelector('.search')
+        container.insertBefore(div, search)
+
+
+    }
+    clearAlert() {
+        let currentAlert = document.querySelector('.alert')
+        if (currentAlert) {
+            currentAlert.remove() // if we do not use this function "user not found" will continuously show whenever we click on submit
+        }
+    }
+}
---
+++ JS/Gitthub_finder.js
@@ -0,0 +1,40 @@
+//linking html with JS
+let search_user_box = document.querySelector('#searchUser')
+let search_btn = document.querySelector('#searchBtn')
+let ui_object = new UI() //UI class object
+
+//addEventListener
+search_btn.addEventListener('click', callApi)
+// define function
+function callApi(e) {
+    //console.log("connected")
+    let user_input = search_user_box.value  //https://api.github.com/users/repos/${user_input}
+    //console.log(user_input)
+    if (user_input != "") {
+        fetch(`https://api.github.com/users/${user_input}`) //here the user name will come,which user want to find
+            .then(result => result.json()) //json format will come
+            .then(data => {
+                //console.log(data)
+                if (data.message == 'Not Found') {
+                    //show alert
+                    //alert("User Not Found")
+                    ui_object.showAlert("User not found", "alert bg-danger text-white")
+                }
+                else {
+                    //show user profile
+                    ui_object.show_user_profile(data) //here all the info will pass from show_user_profile function
+                }
+
+            })
+
+    } else {
+        //alert("Insert User Name Please")
+        ui_object.clear_profile()
+
+    }
+
+
+
+}
+
+
